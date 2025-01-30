ed76914e88a102dfa28ccdbe28ec7dd63e79570e
pichu1298 <62480135+pichu1298@users.noreply.github.com>
2025-01-30 11:53:28 -0500

    sad

--- app/src/components/TheEvents.vue
+++ app/src/components/TheEvents.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <p>Hi</p>
+    <p>{{ events[Math.floor(Math.random() * events.length)] }}</p>
   </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
-let events = reactive({
-  goodLuck: 'You got something.',
-  badLuck: 'Uh oh. You might have lost an item.',
-  confusingLuck: 'You lost something, but found it again.',
-})
+import { ref } from 'vue'
+let events = ref([
+  'You got something.',
+  'Uh oh. You might have lost an item.',
+  'You lost something, but found it again.',
+])
 </script>
 
 <style scoped></style>
